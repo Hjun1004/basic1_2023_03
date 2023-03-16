ea155d703eff4cfdf241ccf0b7e217705ffe18f5
Hjun1004 <hjun1004@naver.com>
2023-03-16 14:27:10 +0900

    Session구현하고 로그인 form형식으로 변ã경

--- src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/basic1/boundedContext/member/controller/MemberController.java
@@ -25,7 +25,33 @@ public class MemberController {
 //    public MemberController(MemberService memberService){
 //        this.memberService = memberService;
 //    }
+
     @GetMapping("/member/login")
+    @ResponseBody
+    public String showLogin(){
+        if(rq.isLogined()){
+            return """
+                    <h1>이미 로그인 되었습니다.<h1>
+                    """.stripIndent();
+        }
+
+        return """
+                <h1>로그인</h1>
+                <form method="GET" action="/member/doLogin" target="_blank">
+                <input type="text" name="username" placeholder="아이디">
+                <input type="text" name="password" placeholder="비밀번호">
+                <input type="submit" value="로그인">
+                </form>
+                """.stripIndent();
+        //순서대로 /member/doLogin 후 ?가 붙는다.
+        // 그 후 username = 입력받는 아이디 입력하면 자동으로 &이 붙는다.
+        // 그리고 password = 입력받는 비밀번호
+        // 버튼으로 로그인 해도되고 <button type="submit">로그인</button>
+
+    }
+
+
+    @GetMapping("/member/doLogin")
     @ResponseBody
     public RsData login(String username, String password){
         if ( username == null || username.trim().length() == 0 ) {
@@ -40,7 +66,7 @@ public class MemberController {
 
         if( rsData.isSuccess()){
             Member member = (Member)rsData.getData();
-            rq.setCookie("loginedMemberId", member.getId());
+            rq.setSession("loginedMemberId", member.getId());
         }
 
         return rsData;
@@ -49,7 +75,7 @@ public class MemberController {
     @GetMapping("/member/me")
     @ResponseBody
     public RsData showMe(){
-        long loginedMemberId = rq.getCookieAsLong("loginedMemberId", 0);
+        long loginedMemberId = rq.getSessionAsLong("loginedMemberId", 0);
 
         if(loginedMemberId == 0){
             return RsData.of("F-1", "로그인 후 이용해주세요.");
@@ -60,9 +86,9 @@ public class MemberController {
         return RsData.of("S-1", "당신의 username은 %s 입니다.".formatted(members.getUsername()));
     }
 
-    @GetMapping("/member/logout")
+    @GetMapping("/member/logout2")
     @ResponseBody
-    public RsData logout(HttpServletRequest req, HttpServletResponse resp) {
+    public RsData logout2(HttpServletRequest req, HttpServletResponse resp) {
         if (req.getCookies() != null) {
             Arrays.stream(req.getCookies())
                     .filter(cookie -> cookie.getName().equals("loginedMemberId"))
@@ -75,12 +101,19 @@ public class MemberController {
         return RsData.of("S-1", "로그아웃 되었습니다.");
     }
 
-    @GetMapping("/member/logout2")
+    @GetMapping("/member/logout")
     @ResponseBody
-    public RsData logout2() {
-        boolean cookieRemoved = rq.removeCookie("loginedMemberId");
+    public RsData logout1() {
+        boolean sessionRemoved = rq.removeSession("loginedMemberId");
 
-        if(cookieRemoved == false) return RsData.of("F-5", "이미 로그아웃");
+        if(sessionRemoved == false) return RsData.of("F-5", "이미 로그아웃");
         return RsData.of("S-1", "로그아웃 되었습니다.");
     }
+
+    // 디버깅용 함수
+//    @GetMapping("/member/session")
+//    @ResponseBody
+//    public String showSession() {
+//        return rq.getSessionDebugContents().replaceAll("\n", "<br>");
+//    }
 }
