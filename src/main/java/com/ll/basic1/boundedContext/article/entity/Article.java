9dd45819ab96c9b04b2c396db0f059782581e20d
Hjun1004 <hjun1004@naver.com>
2023-03-17 15:34:17 +0900

    등록날짜, 수정날짜 자동갱신

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,10 +1,10 @@
 package com.ll.basic1.boundedContext.article.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,12 +15,15 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id // id가 메인키다. 주된 키다.
-    @GeneratedValue(strategy = IDENTITY)
+    @GeneratedValue(strategy = IDENTITY) // Auto_INCREMENT
     private long id;
 
+    @CreatedDate
     private LocalDateTime createDate; // 시간을 저장하기 위한 자료형으로 LocalDateTime으로 설정
+    @LastModifiedDate  // 이렇게 하면 LocalDateTime.now()
     private LocalDateTime modifyDate;
     private String title;
     private String body;
